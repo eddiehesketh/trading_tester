6802be3ce0d115c3133b5452df67d6d728505aa6
eddiehesketh <tsehesketh@gmail.com>
2024-09-13 14:45:54 +0930

    s

--- ReadData.cpp
+++ ReadData.cpp
@@ -3,4 +3,13 @@
 
 using namespace std;
 
-ReadData::ReadData(int _index): this->index = index {};
+ReadData::ReadData(int _index): index(_index){};
+ReadData::ReadData(): ReadData(0){};
+
+
+void ReadData::addElement(int price){open_prices.push_back(price);}
+
+void ReadData::readOpenPrices(string filename){
+    ifstream file(filename);
+    
+}
