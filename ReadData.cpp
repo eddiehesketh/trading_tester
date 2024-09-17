24c3fc9077ea58c0fb4e6c2cf8512508db8c80c3
eddiehesketh <tsehesketh@gmail.com>
2024-09-17 20:46:00 +0930

    part way through finishing commenting

--- ReadData.cpp
+++ ReadData.cpp
@@ -3,21 +3,31 @@
 
 using namespace std;
 
-
+// Read data class constructor.
 ReadData::ReadData(string stock_info){
+
+    // Initialising stock data based off name of csv file.
     set_stock_data(stock_info);
+    
+    // Using set titles function to allow getline to act on data points instead of the coloumn titles.
     set_titles();
 
+    // Initialising line variable, which will change each while loop iteration.
     string line = "";
 
+        // While loop iterating over each row.
         while (getline(stock_data, line)) {
+
+            // Using stringstream to seperate the line extracted from getline function into into individual sets of numbers.
             stringstream ss(line);
-            set_dates(ss);
-            set_stock_open(ss);
-            set_stock_close(ss);
-            set_stock_high(ss);
-            set_stock_low(ss);
-            set_stock_volume(ss);
+
+            // Passing stringstream into subsequent data set functions.
+            set_dates(ss); // Setting dates.
+            set_stock_open(ss); // Setting open prices.
+            set_stock_close(ss); // Setting close prices.
+            set_stock_high(ss); // Setting high price.
+            set_stock_low(ss); // Setting low price.
+            set_stock_volume(ss); // Setting dailty volume.
 
     }
 
@@ -67,20 +77,28 @@ string ReadData::clear_commas(string value){
 
 }
 
+// Set stock data function opens the file passed into the class.
 void ReadData::set_stock_data(string stock_file_name){
-    this->stock_data.open(stock_file_name);
+    this->stock_data.open(stock_file_name); // Open the csv file passed in.
 }
 
+// Get stock data function returns stock data by reference.
 ifstream& ReadData::get_stock_data(){
-    return this->stock_data;
+    return this->stock_data; // Returns protected stock data.
 }
 
+// set titles function called first in constructor to extract titles, and allow the other data to be focused on.
 void ReadData::set_titles(){
-    getline(get_stock_data(), this->titles);
+    getline(get_stock_data(), this->titles); // Use getline function to extract the titles.
 }
 
+// Set dates function takes in ss stringstream and appends date onto date vector.
 void ReadData::set_dates(stringstream& ss){
+    
+    // Set temporary data string.
     string running_date;
+
+    // Use getline to extract info from the end of most recent getline call to the next comma, and store it in the running date variavle
     getline(ss, running_date, ',');
     date.push_back(clean_value(running_date));
 }
