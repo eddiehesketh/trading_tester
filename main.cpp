d1a86abacdf7a783666b13d2424002592b02dab5
eddiehesketh <tsehesketh@gmail.com>
2024-09-30 12:25:31 +0930

    testing

--- main.cpp
+++ main.cpp
@@ -11,5 +11,11 @@ int main(){
 
     ReadData* stock1 = new ReadData("test_csvfile.csv");
 
+    // for (size_t i = 0; i < stock1->get_dates().size(); i++) {
+    //     cout << "Date: " << stock1->get_dates()[i] << ", Open: " << stock1->get_open_prices()[i] << endl;
+    // }
+
+
+
     return 0;
 }
