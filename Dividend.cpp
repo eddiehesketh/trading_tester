e87793df35e949371f4f37237052b889574f36e9
eddiehesketh <tsehesketh@gmail.com>
2024-10-03 17:37:08 +0930

    good

--- Dividend.cpp
+++ Dividend.cpp
@@ -41,7 +41,42 @@ bool Dividend::valid_start_date(string start){
 
     string current_date = get_dates().back();
 
-    
+    int index;
+
+    for (int i = 0; i < get_dates().size(); i++){
+
+
+        if (start == get_dates()[i]){
+
+            index = i;
+
+            if (get_pay_freq() == "Monthly"){
+
+                if (get_dates().size()-1-index >= 30){
+
+                    set_index(index);
+
+                    set_start_date(start);
+
+                    return true;
+                }
+            }
+
+            if (get_pay_freq() == "Quarterly"){
+
+                if (get_dates().size()-1-index >= 92){
+
+                    set_index(index);
+
+                    set_start_date(start);
+
+                    return true;
+                }
+            }
+
+            
+        }
+    }
 
 }
 
