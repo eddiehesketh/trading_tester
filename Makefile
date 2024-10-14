30a723fb2b37f7ec4b4a1c6c2e6a0459fbf4be68
eddiehesketh <tsehesketh@gmail.com>
2024-10-14 12:06:25 +1030

    makefile

---
+++ Makefile
@@ -0,0 +1,14 @@
+
+test: all_unit_testing.cpp ReadData.cpp SetDeposit.cpp Dividend.cpp Display.cpp Investment.cpp
+	@echo "Compiling test program..."
+	@g++ -std=c++11 all_unit_testing.cpp ReadData.cpp SetDeposit.cpp Dividend.cpp Display.cpp Investment.cpp -o test
+	@echo "Running test..."
+	@./test
+
+
+clean:
+	@echo "Cleaning up..."
+	@rm -f test
+
+
+all: test
--- all_unit_testing.cpp
+++ all_unit_testing.cpp
@@ -13,30 +13,38 @@ using namespace std;
 
 int main(){
 
-
+    // Tests for read data.
     cout << "READ DATA TESTING: \n\n\n";
 
+    // Calling read data test constructor.
     ReadDataTest test1;
 
+    // Running tests.
     test1.run_tests();
 
+     // Tests for set depost.
     cout << "\n\n\nSET DEPOSIT TESTING: \n\n\n";
 
     SetDepsoitTest test2;
 
+     // Running tests.
     test2.run_tests();
 
+     // Tests for dividend.
     cout << "\n\n\nDIVIDEND TESTING: \n\n\n";
 
     DividendTest test3;
 
+     // Running tests.
     test3.run_tests();
 
 
+     // Tests for display data.
     cout << "\n\n\n DISPLAY DATA TESTING: \n\n\n";
 
     DisplayTest test4;
 
+     // Running tests.
     test4.run_tests();
 
 
