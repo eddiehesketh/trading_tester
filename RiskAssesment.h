365eb8f810052a8ca3a90831652e0139d54bbbba
eddiehesketh <tsehesketh@gmail.com>
2024-10-14 16:13:22 +1030

    risk class done

---
+++ RiskAssesment.h
@@ -0,0 +1,34 @@
+// Risk assesment class provides a risk rating based on the type of investment.
+
+// Defining the class and including the relevant investment header file.
+#ifndef RISKASSESMENT_H
+#define RISKASSESMENT_H
+#include "Investment.h"
+
+
+// Risk assesment class.
+class RiskAssesment{
+
+    protected:
+
+        // Risk status variable.
+        std::string risk_rating;
+
+    public:
+
+        // Risk assesment constructor, taking in an investment pointer.
+        RiskAssesment(Investment* stock);
+
+        // Risk assesment function which will determine the risk of an investment.
+        void risk_assesment(Investment* p);
+
+        // Setter for the risk rating.
+        void set_risk_rating(std::string rating);
+
+        // Getter for the risk rating.
+        std::string get_risk_rating();
+
+};
+
+
+#endif
